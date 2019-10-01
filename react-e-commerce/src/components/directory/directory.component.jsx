43b1b4589a5dbd1d25be28a708261321566a969f
Bartosz Butrym <bartoszbutrym@sourceful.nl>
2019-10-01 16:13:29 +0200

    add react router

--- react-e-commerce/src/components/directory/directory.component.jsx
+++ react-e-commerce/src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import MenuItem from "../menu-item/menu-item.component";
-import './directory.styles.scss';
+import "./directory.styles.scss";
 
 class Directory extends Component {
   constructor() {
@@ -11,7 +11,7 @@ class Directory extends Component {
           title: "hats",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1,
-          linkUrl: "shop/hats"
+          linkUrl: "hats"
         },
         {
           title: "jackets",
@@ -42,12 +42,18 @@ class Directory extends Component {
       ]
     };
   }
-  
+
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => (
+          <MenuItem
+            key={id}
+            title={title}
+            imageUrl={imageUrl}
+            size={size}
+            linkUrl={linkUrl}
+          />
         ))}
       </div>
     );
